70fe08d8d9c1a4135babfc97cb0407464a7ded68
Saulo Coimbra <saulo.coimbra1@gmail.com>
2023-12-29 17:32:07 -0300

    Create components and pages

--- movies_lib/src/App.jsx
+++ movies_lib/src/App.jsx
@@ -1,24 +1,15 @@
-
-import { Link, Outlet } from 'react-router-dom'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  
-
   return (
-<div className='App'>
-<nav id='navbar'>
-<h2>
-  <Link to="/">MoviesLib</Link>
-  <Link to="/movie/1">MoviesLib</Link>
-  <Link to="/search">MoviesLib</Link>
-  
-</h2>
-</nav>
-<h2>Movies Lib</h2>
-<Outlet/>
-</div>
-  )
+    <div className="App">
+      <Navbar />
+      <h2>Movies Lib</h2>
+      <Outlet />
+    </div>
+  );
 }
 
-export default App
+export default App;
